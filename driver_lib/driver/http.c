95588566369e7797b3d0812bb1bd1838c033ee5e
17harsh <harshsinghal17@yahoo.in>
2021-06-19 22:52:57 +0530

    Refactored and improved HTTP Library

--- driver_lib/driver/http.c
+++ driver_lib/driver/http.c
@@ -23,109 +23,19 @@
 	#define HTTP_LOG_DEBUG_ARGS(message, args...)	do {os_printf("[HTTP-DEBUG] "); os_printf(message, args); os_printf("\r\n");} while(0)
 #endif
 
-//** Private Methods **********************************************************************************//
-
-HTTP_RESULT _httpRoutePath(char *iRecv, uint16 iLength, char **oRoutePath, uint16 *oPathLength);
-HTTP_RESULT _httpRequestData(char *iRecv, uint16 iLength, char **oData, uint16 *oDataLength);
-
-//****************************************************************************************************//
-
-HTTP_RESULT isHttp(char *iRecv, uint16 iLength){
-	HTTP_RESULT result = HTTP_FAIL;
-	if(iLength > 0 && iRecv != NULL){
-		char *http = NULL;
-		http = os_strstr(iRecv, "HTTP");
-		if(http != NULL) result = HTTP_SUCCESS;
-	}
-	return result;
-}
-
-HTTP_RESULT processHttpRequest (char *iRecv, uint16 iLength, HTTP_REQUEST *oHttpRequest){
-	HTTP_RESULT result = HTTP_FAIL;
-	if(iRecv != NULL && iLength > 0 && oHttpRequest != NULL){
-
-		HTTP_REQUEST_TYPE httpRequest = HTTP_INVALID;
-		if(os_strncmp(iRecv, "GET", 3) == 0){
-			httpRequest =  HTTP_GET;
-			HTTP_LOG_DEBUG("Request type : GET");
-		}
-		else if(os_strncmp(iRecv, "POST", 4) == 0){
-			httpRequest =  HTTP_POST;
-			HTTP_LOG_DEBUG("Request type : POST");
-		}
-
-		oHttpRequest->httpRequestType = httpRequest;
-
-		if(httpRequest != HTTP_INVALID){
-			char *routePath = NULL;
-			uint16 pathLength = -1;
-			if(_httpRoutePath(iRecv, iLength, &routePath, &pathLength) == HTTP_SUCCESS){
-				if(pathLength == 0 && routePath != NULL){
-					//that is, route path is root
-					++pathLength;
-					--routePath;
-				}
-				oHttpRequest->routeLength = pathLength;
-				oHttpRequest->routePath = routePath;
-				result = HTTP_SUCCESS;
-			}
-
-			char *data = NULL;
-			uint16 dataLength = -1;
-			if(_httpRequestData(iRecv, iLength, &data, &dataLength) == HTTP_SUCCESS){
-				oHttpRequest->dataLength = dataLength;
-				oHttpRequest->data = data;
-				result = HTTP_SUCCESS;
-			}
-		}
-	}
-	return result;
-}
-
-HTTP_RESULT sendHttpResponse (struct espconn *espconn, HTTP_RESPONSE* iHttpResponse){
-	HTTP_LOG_DEBUG("inside sendHttpResponse");
-	HTTP_RESULT result = HTTP_FAIL;
-	if(iHttpResponse != NULL){
-		HTTP_LOG_DEBUG_ARGS("content : %s", iHttpResponse->content);
-
-		char* responsePacket = (char*) os_zalloc(256 + iHttpResponse->contentLength);
-
-		char *responseCode = NULL;
-		if(iHttpResponse->responseCode == HTTP_OK) responseCode = "200 OK";
-		else if(iHttpResponse->responseCode == HTTP_Bad_Request) responseCode = "400 Bad Request";
-		else if(iHttpResponse->responseCode == HTTP_Not_Found) responseCode = "404 Not Found";
-
-		char *contentType = NULL;
-		if(iHttpResponse->contentType == text_html) contentType = "text/html";
-		else if(iHttpResponse->contentType == text_css) contentType = "text/css";
-		else if(iHttpResponse->contentType == application_javascript) contentType = "application/javascript";
-
-		char *connection = NULL;
-		if(iHttpResponse->connection == Closed) connection = "Closed";
-
-		if(responsePacket != NULL && responseCode != NULL && contentType != NULL && connection !=NULL && iHttpResponse->content != NULL){
-			os_sprintf(responsePacket, "HTTP/1.1 %s\r\n\
-Server: ESP8266\r\n\
-Content-Length: %d\r\n\
-Content-Type: %s\r\n\
-Connection: %s\r\n\r\n\
-%s", responseCode, iHttpResponse->contentLength, contentType, connection, iHttpResponse->content);
-
-			HTTP_LOG_DEBUG_ARGS("Content : %s",responsePacket);
-			if(espconn != NULL){
-				sint8 status = espconn_send(espconn, responsePacket, os_strlen(responsePacket));
-				HTTP_LOG_DEBUG_ARGS("espconn send, status : %d",status);
-				if(status == 0) result = HTTP_SUCCESS;
-			}
-		}
-		os_free(responsePacket);
-	}
-	return result;
-}
-
-HTTP_RESULT _httpRoutePath(char *iRecv, uint16 iLength, char **oRoutePath, uint16 *oPathLength){
+/***********************************************************************************
+ * FunctionName : _httpRoutePath
+ * Description  : Extract HTTP route path from raw data.
+ * Parameters   : iRecv		   -- raw received data
+ *                iLength	   -- length of received data
+ *                oRoutePath   -- route path
+ *                oPathLength  -- route path length
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool _httpRoutePath(char *iRecv, uint16 iLength, char **oRoutePath, uint16 *oPathLength){
 	HTTP_LOG_DEBUG("Inside httpRoutePath");
-	HTTP_RESULT result = HTTP_FAIL;
+	bool result = false;
 	if(iRecv != NULL && iLength > 0){
 		char* p1 = NULL;
 		char* p2 = NULL;
@@ -135,15 +45,25 @@ HTTP_RESULT _httpRoutePath(char *iRecv, uint16 iLength, char **oRoutePath, uint1
 			++p1;--p2;
 			*oRoutePath = p1;
 			*oPathLength = p2 - p1;
-			result = HTTP_SUCCESS;
+			result = true;
 		}
 	}
 	return result;
 }
 
-HTTP_RESULT _httpRequestData(char *iRecv, uint16 iLength, char **oData, uint16 *oDataLength){
+/***********************************************************************************
+ * FunctionName : _httpRequestData
+ * Description  : Extract HTTP request data from raw data.
+ * Parameters   : iRecv		   -- raw received data
+ *                iLength	   -- length of received data
+ *                oData		   -- extracted HTTP data
+ *                oDataLength  -- HTTP data length
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool _httpRequestData(char *iRecv, uint16 iLength, char **oData, uint16 *oDataLength){
 	HTTP_LOG_DEBUG("Inside httpRequestData");
-	HTTP_RESULT result = HTTP_FAIL;
+	bool result = false;
 	if(iRecv != NULL && iLength > 0){
 		char* p1 = NULL;
 		char* p2 = NULL;
@@ -159,9 +79,134 @@ HTTP_RESULT _httpRequestData(char *iRecv, uint16 iLength, char **oData, uint16 *
 			os_memcpy(p4, p2, p3-p2);
 			uint16 datalength = atoi(p4);
 			*oDataLength = datalength;
-			result = HTTP_SUCCESS;
+			result = true;
 		}
 	}
 	return result;
 }
 
+/***********************************************************************************
+ * FunctionName : sendHttpResponse
+ * Description  : Send Http response to server.
+ * Parameters   : espconn	    -- esp connection obj
+ *                iHttpResponse -- HTTP reponse obj
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool sendHttpResponse (struct espconn *espconn, HTTP_RESPONSE_PACKET* iHttpResponse){
+	HTTP_LOG_DEBUG("inside sendHttpResponse");
+	bool result = false;
+	if(iHttpResponse != NULL){
+		HTTP_LOG_DEBUG_ARGS("content : %s", iHttpResponse->content);
+
+		char* responsePacket = (char*) os_zalloc(256 + iHttpResponse->contentLength);
+
+		char *httpStatusCode = NULL;
+		if(iHttpResponse->httpStatusCode == HTTP_OK) httpStatusCode = "200 OK";
+		else if(iHttpResponse->httpStatusCode == HTTP_Bad_Request) httpStatusCode = "400 Bad Request";
+		else if(iHttpResponse->httpStatusCode == HTTP_Not_Found) httpStatusCode = "404 Not Found";
+
+		char *contentType = NULL;
+		if(iHttpResponse->contentType == text_html) contentType = "text/html";
+		else if(iHttpResponse->contentType == text_css) contentType = "text/css";
+		else if(iHttpResponse->contentType == application_javascript) contentType = "application/javascript";
+
+		char *connection = NULL;
+		if(iHttpResponse->connection == Closed) connection = "Closed";
+
+		if(responsePacket != NULL && httpStatusCode != NULL && contentType != NULL && connection !=NULL && iHttpResponse->content != NULL){
+			os_sprintf(responsePacket, "HTTP/1.1 %s\r\n\
+Server: ESP8266\r\n\
+Content-Length: %d\r\n\
+Content-Type: %s\r\n\
+Connection: %s\r\n\r\n\
+%s", httpStatusCode, iHttpResponse->contentLength, contentType, connection, iHttpResponse->content);
+
+			HTTP_LOG_DEBUG_ARGS("Content : %s",responsePacket);
+			if(espconn != NULL){
+				sint8 status = espconn_send(espconn, responsePacket, os_strlen(responsePacket));
+				HTTP_LOG_DEBUG_ARGS("espconn send, status : %d",status);
+				if(status == 0) result = true;
+			}
+		}
+		os_free(responsePacket);
+	}
+	return result;
+}
+
+/***********************************************************************************
+ * FunctionName : processHttpRequest
+ * Description  : process raw received HTTP Request Data
+ * Parameters   : iRecv	    	-- received data
+ *                iLength  		-- length of received data
+ *                oHttpRequest  -- Processed Http Request Obj
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool processHttpRequest (char *iRecv, uint16 iLength, HTTP_REQUEST_PACKET *oHttpRequest){
+	bool result = false;
+	if(iRecv != NULL && iLength > 0 && oHttpRequest != NULL){
+
+		HTTP_METHOD httpRequest = HTTP_INVALID;
+		if(os_strncmp(iRecv, "GET", 3) == 0){
+			httpRequest =  HTTP_GET;
+			HTTP_LOG_DEBUG("Request type : GET");
+		}
+		else if(os_strncmp(iRecv, "POST", 4) == 0){
+			httpRequest =  HTTP_POST;
+			HTTP_LOG_DEBUG("Request type : POST");
+		}
+
+		oHttpRequest->httpMethod = httpRequest;
+
+		if(httpRequest != HTTP_INVALID){
+			char *routePath = NULL;
+			uint16 pathLength = -1;
+			if(_httpRoutePath(iRecv, iLength, &routePath, &pathLength) == true){
+				if(pathLength == 0 && routePath != NULL){
+					//that is, route path is root
+					++pathLength;
+					--routePath;
+				}
+				oHttpRequest->routeLength = pathLength;
+				oHttpRequest->routePath = routePath;
+				result = true;
+			}
+
+			char *data = NULL;
+			uint16 dataLength = -1;
+			if(_httpRequestData(iRecv, iLength, &data, &dataLength) == true){
+				oHttpRequest->dataLength = dataLength;
+				oHttpRequest->data = data;
+				result = true;
+			}
+		}
+	}
+	return result;
+}
+
+/***********************************************************************************
+ * FunctionName : isHttp
+ * Description  : check if received data is of HTTP Protocol and its message type
+ * Parameters   : iRecv	   -- received data
+ *                iLength  -- length of received data
+ *                oMsgType -- Http Message Type
+ * Returns      : bool	-- true if data is of HTTP Protocol
+ * 						-- false if data is not of HTTP Protocol
+***********************************************************************************/
+bool isHttp(char *iRecv, uint16 iLength, HTTP_MESSAGE_TYPE *oMsgType){
+	bool result = false;
+	if(iLength > 0 && iRecv != NULL){
+		char *isHttp = NULL;
+		isHttp = os_strstr(iRecv, "HTTP");
+		if(isHttp != NULL){
+			result = true;
+
+			if(os_strncmp(iRecv, "HTTP", 4) == 0)
+				*oMsgType = HTTP_RESPONSE;
+			else
+				*oMsgType = HTTP_REQUEST;
+		}
+	}
+	return result;
+}
--- driver_lib/include/driver/http.h
+++ driver_lib/include/driver/http.h
@@ -16,16 +16,11 @@ struct espconn;
 //un-comment this for debugging messages
 //#define HTTP_DEBUG
 
-typedef enum httpResult {
-	HTTP_FAIL,
-	HTTP_SUCCESS
-}HTTP_RESULT;
-
-typedef enum httpResponseCode {
+typedef enum httpStatusCode {
 	HTTP_OK, //200,
 	HTTP_Bad_Request, //400,
 	HTTP_Not_Found  //404
-}HTTP_RESPONSE_CODE;
+}HTTP_STATUS_CODE;
 
 typedef enum connectionState{
 	Closed,
@@ -39,41 +34,62 @@ typedef enum contentType{
 }CONTENT_TYPE;
 
 typedef struct httpResponse{
-	HTTP_RESPONSE_CODE responseCode;
+	HTTP_STATUS_CODE httpStatusCode;
 	CONNECTION connection;
 	char *content;
 	uint16 contentLength;
 	CONTENT_TYPE contentType;
-} HTTP_RESPONSE;
+} HTTP_RESPONSE_PACKET;
 
-typedef enum httpRequestType {
+typedef enum httpMethod {
 	HTTP_GET,
 	HTTP_POST,
 	HTTP_INVALID
-}HTTP_REQUEST_TYPE ;
+}HTTP_METHOD ;
 
 typedef struct httpRequest{
-	HTTP_REQUEST_TYPE httpRequestType;
+	HTTP_METHOD httpMethod;
 	char *routePath;
 	uint16 routeLength;
 	char *data;
 	uint16 dataLength;
-} HTTP_REQUEST;
+} HTTP_REQUEST_PACKET;
 
+typedef enum httpMessageType{
+	HTTP_REQUEST,
+	HTTP_RESPONSE
+} HTTP_MESSAGE_TYPE;
 
+/***********************************************************************************
+ * FunctionName : isHttp
+ * Description  : check if received data is of HTTP Protocol and its message type
+ * Parameters   : iRecv	   -- received data
+ *                iLength  -- length of received data
+ *                oMsgType -- Http Message Type
+ * Returns      : bool	-- true if data is of HTTP Protocol
+ * 						-- false if data is not of HTTP Protocol
+***********************************************************************************/
+bool isHttp(char *iRecv, uint16 iLength, HTTP_MESSAGE_TYPE *oMsgType);
 
-/******************************************************************************
- * FunctionName : is_http
- * Description  : check if received data is of http type
- * Parameters   : precv	-- data received
- *                length -- length of received data
- * Returns      : bool	-- true if data is http type
- * 						-- false if data is not http type
-*******************************************************************************/
-HTTP_RESULT isHttp(char *iRecv, uint16 iLength);
+/***********************************************************************************
+ * FunctionName : processHttpRequest
+ * Description  : process raw received HTTP Request Data from server
+ * Parameters   : iRecv	    	-- received data
+ *                iLength  		-- length of received data
+ *                oHttpRequest  -- Processed Http Request Obj
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool processHttpRequest (char *iRecv, uint16 iLength, HTTP_REQUEST_PACKET *oHttpRequest);
 
-HTTP_RESULT processHttpRequest (char *iRecv, uint16 iLength, HTTP_REQUEST *oHttpRequest);
-
-HTTP_RESULT sendHttpResponse (struct espconn *espconn, HTTP_RESPONSE* iHttpResponse);
+/***********************************************************************************
+ * FunctionName : sendHttpResponse
+ * Description  : Send Http response to server.
+ * Parameters   : espconn	    -- esp connection obj
+ *                iHttpResponse -- HTTP reponse obj
+ * Returns      : bool	-- true if Successful
+ * 						-- false if Failed
+***********************************************************************************/
+bool sendHttpResponse (struct espconn *espconn, HTTP_RESPONSE_PACKET* iHttpResponse);
 
 #endif /* INCLUDE_DRIVER_HTTP_H_ */
